4ff263faf190fe8e61fc442679c7824efb9fa3b6
hanmingqing <h823644206@163.com>
2024-06-07 16:34:02 +0800

    feat：new page

--- js/index.js
+++ js/index.js
@@ -9,12 +9,14 @@ var canvas = document.getElementById('canvas'),
   count = 0,
   maxStars = w; // 星星数量
 canvas.height = window.innerHeight / 10 * 6;
-var canvas2 = document.createElement('canvas'),
-  ctx2 = canvas2.getContext('2d');
-  canvas2.width = 100;
-  canvas2.height = 100;
-var half = canvas2.width / 2,
-  gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+var canvas2 = document.createElement('canvas');
+var ctx2 = canvas2.getContext('2d');
+canvas2.width = 100;
+canvas2.height = 100;
+// 绘制放射性渐变
+var half = canvas2.width / 2;
+var gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+// 添加一个由偏移值和颜色值指定的断点到渐变
 gradient2.addColorStop(0.025, '#CCC');
 gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
 // gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
@@ -40,22 +42,22 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// 星星移动范围，值越大范围越小
 function maxOrbit(x, y) {
-  var max = Math.max(x, y),
-    diameter = Math.round(Math.sqrt(max * max + max * max));
+  var max = Math.max(x, y);
+  var diameter = Math.round(Math.sqrt(max * max + max * max));
   return diameter / 2;
-  //星星移动范围，值越大范围越小，
 }
 
 var Star = function() {
   this.orbitRadius = random(maxOrbit(w, h));
-  this.radius = random(60, this.orbitRadius) / 18;
   // 星星大小
+  this.radius = random(60, this.orbitRadius) / 18;
   this.orbitX = w / 2;
   this.orbitY = h / 2;
   this.timePassed = random(0, maxStars);
+  // 星星移动速度
   this.speed = random(this.orbitRadius) / 500000;
-  //星星移动速度
   this.alpha = random(2, 10) / 10;
 
   count++;
@@ -66,13 +68,13 @@ Star.prototype.draw = function () {
   var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
     y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
     twinkle = random(10);
-
+  // 星星闪烁
   if (twinkle === 1 && this.alpha > 0) {
     this.alpha -= 0.05;
   } else if (twinkle === 2 && this.alpha < 1) {
     this.alpha += 0.05;
   }
-
+  // 星星透明度
   ctx.globalAlpha = this.alpha;
   ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
   // this.timePassed += this.speed;
